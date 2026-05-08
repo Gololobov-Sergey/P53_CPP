05b6993c712589fca96fd3dc17afb0dec3f2881f
gololobov <gololobov.serhiy@gmail.com>
2026-05-08 20:40:47 +0300

    Добавьте файлы проекта.

---
+++ P53_CPP/Source.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+#include<Windows.h>
+
+using namespace std;
+
+int main()
+{
+	SetConsoleCP(65001); 
+	SetConsoleOutputCP(65001);
+
+
+	cout << "Hello World!" << endl;
+	cout << "Мене звати Сергій" << endl;
+}
